91a846927dceb8a291acefc2d5553ec9a6c5661f
Nathan Bengio <Nathan Bengio@LAPTOP-PI06HU2U>
2025-01-20 22:28:21 +0100

    Ajoutez des fichiers projet.

---
+++ MediaTekDocuments/model/Commande.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MediaTekDocuments.model
+{
+    /// <summary>
+    /// Classe métier Commande
+    /// </summary>
+    public class Commande
+    {
+        public string id { get; set; }
+        public DateTime dateCommande { get; set; }
+        public double montant { get; set; }
+
+        public Commande(string id, DateTime dateCommande, double montant)
+        {
+            this.id = id;
+            this.dateCommande = dateCommande;
+            this.montant = montant;
+        }
+
+    }
+
+}
---
+++ MediaTekDocuments/model/Suivi.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace MediaTekDocuments.model
+{
+    /// <summary>
+    /// Classe suivi de commande
+    /// </summary>
+    public class Suivi
+    {
+        public string id { get; set; }
+
+        public string etapeSuivi { get; set; }
+
+        public Suivi (string id, string etapeSuivi)
+        {
+            this.id = id;
+            this.etapeSuivi = etapeSuivi;
+        }
+    }
+}
